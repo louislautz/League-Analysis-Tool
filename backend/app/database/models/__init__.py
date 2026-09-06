cadb791ed9a4ae4de3052ea4586019721685f3fc
louislautz <louislautz@gmx.net>
2026-09-06 12:27:43 +0200

    Added __init__.py to models folder

---
+++ backend/app/database/models/__init__.py
@@ -0,0 +1,5 @@
+from app.database.models.player import Player
+from app.database.models.match import Match
+from app.database.models.player_match import PlayerMatch
+from app.database.models.rank_snapshot import RankSnapshot
+from app.database.models.sync_state import SyncState
